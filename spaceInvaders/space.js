80d0347d3b62c91d92f0a67c8a447dca0e96e3f1
cunnellp5 <philip.cunnell@colorado.edu>
2023-10-29 13:13:13 -0600

    add everything

--- spaceInvaders/space.js
+++ spaceInvaders/space.js
@@ -70,21 +70,15 @@ class Projectile {
         this.radius = 5;
         this.speedX = 1;
         this.speedY = 1;
-        this.speedModifier = 2;
+        this.speedModifier = 5;
         this.free = true;
         this.isPeen = false;
         this.angle = 0;
         this.angleModifier = (45 * Math.PI / 180);
-        this.debug = false;
 
         window.addEventListener('keyup', e => {
             if(e.key === 'p') {
                 this.isPeen = !this.isPeen;
-            }
-        })
-        window.addEventListener('keyup', e => {
-            if(e.key === 'd') {
-                this.debug = !this.debug;
             }
         })
     }
@@ -108,10 +102,14 @@ class Projectile {
             context.save();
             context.translate(this.x, this.y)
             context.rotate((Math.atan2(this.speedY, this.speedX)) + this.angleModifier);
-            context.drawImage(document.getElementById('penis'), -this.radius*5, -this.radius*5);
+            context.drawImage(
+                document.getElementById('penis'),
+                -this.radius * 5,
+                -this.radius * 5,
+            );
             context.restore();
 
-            if (this.debug) {
+            if (this.game.debug) {
                 context.beginPath();
                 context.arc(this.x, this.y, this.radius * 5, 0, Math.PI * 2);
                 context.stroke();
@@ -154,37 +152,72 @@ class Enemy {
         this.height = this.radius * 2;
         this.speedX = 0;
         this.speedY = 0;
+        this.speedModifier = Math.random() * 0.5 + 0.1;
+        this.angle = 0;
+        this.collided = false;
         this.free = true;
     }
 
     start () {
         this.free = false;
+        this.collided = false;
+        this.frameX = 0;
+        this.lives = this.maxLives;
+        // this can lead to issues if other enemy sprite sheets dont have 4 sprite rows
+        this.frameY = Math.floor(Math.random() * 4);
+
         if (Math.random() < 0.5) {
             this.x = Math.random() * this.game.width;
             this.y = Math.random() < 0.5 
                 ? -this.radius
-                : this.game.height - this.radius;
+                : this.game.height + this.radius;
         } else {
             this.x = Math.random() < 0.5 
                 ? -this.radius
-                : this.game.width - this.radius;
+                : this.game.width + this.radius;
             this.y = Math.random() * this.game.height;
         }
-        // calc aim afterr position of enemies
+        // calc aim after position of enemies
         const aim = this.game.calcAim(this, this.game.planet);
-        this.speedX = aim[0];
-        this.speedY = aim[1];
+        this.speedX = aim[0] * this.speedModifier;
+        this.speedY = aim[1] * this.speedModifier;
+        this.angle = Math.atan2(aim[3], aim[2]) + Math.PI * 0.5;
     }
 
     reset () {
         this.free = true;
     }
 
+    hit (damage) {
+        this.lives -= damage;
+        if (this.lives >= 1) {
+            this.frameX++;
+        }
+    }
+
     draw (context) {
         if (!this.free) {
-            context.beginPath();
-            context.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
-            context.stroke();
+            context.save();
+            context.translate(this.x, this.y);
+            context.rotate(this.angle);
+            context.drawImage(
+                this.image,
+                this.frameX * this.width,
+                this.frameY * this.height,
+                this.width,
+                this.height,
+                -this.radius,
+                -this.radius,
+                this.width,
+                this.height
+            );
+            if (this.game.debug) {
+                context.beginPath();
+                context.arc(0, 0, this.radius, 0, Math.PI * 2);
+                context.stroke();
+                context.fillText(this.lives, 0, 0);
+            }
+            context.restore();
         }
     }
 
@@ -193,29 +226,76 @@ class Enemy {
             this.x += this.speedX;
             this.y += this.speedY;
 
-            // collision enemy/planet
-            if (this.game.checkCollision(this, this.game.planet)) {
-                this.reset();
+            // check collision enemy/planet
+            if (this.game.checkCollision(this, this.game.planet) && this.lives >= 1) {
+                this.lives = 0;
+                this.speedX = 0;
+                this.speedY = 0;
+                this.collided = true;
+                this.game.lives--;
+            }
+            
+            // check collision enemy/player
+            if (this.game.checkCollision(this, this.game.player) && this.lives >= 1) {
+                this.lives = 0;
+                this.collided = true;
+                this.game.lives--;
             }
 
-            // collision enemy/player
-            if (this.game.checkCollision(this, this.game.player)) {
-                this.reset();
-            }
-
-            // check enemy/projectiole
+            // check collision enemy/projectile
             this.game.projectilePool.forEach((projectile) => {
                 if (!projectile.free) {
-                    if (this.game.checkCollision(this, projectile)) {
+                    if (this.game.checkCollision(this, projectile) && this.lives >= 1) {
                         projectile.reset();
-                        this.reset();
+                        this.hit(1)
                     }
                 }
             })
+
+            // sprite animation
+            if (this.lives < 1 && this.game.spriteUpdate) {
+                this.frameX++;
+            }
+
+            if (this.frameX > this.maxFrame) {
+                this.reset();
+                if (!this.collided) {
+                    this.game.score += this.maxLives;
+                }
+            }
+
+            // screen animation
+            // stretch goal here
         }
     }
-
 }
+
+
+class Asteroid extends Enemy {
+    constructor (game) {
+        super(game);
+        this.image = document.getElementById('asteroid');
+        this.frameX = 0;
+        this.frameY = Math.floor(Math.random() * 4);
+        this.maxFrame = 7;
+        this.lives = 1;
+        this.maxLives = this.lives;
+    }
+}
+
+
+class LobsterMorph extends Enemy {
+    constructor (game) {
+        super(game);
+        this.image = document.getElementById('lobsterMorph');
+        this.frameX = 0;
+        this.frameY = Math.floor(Math.random() * 4);
+        this.maxFrame = 14;
+        this.lives = 8;
+        this.maxLives = this.lives;
+    }
+}
+
 
 class Game {
     constructor (canvas) {
@@ -237,7 +317,17 @@ class Game {
         this.createEnemyPool();
         this.enemyPool[0].start();
         this.enemyTimer = 0;
-        this.enemyInterval = 500;
+        this.enemyInterval = 1000;
+
+        // sprite sheet
+        this.spriteUpdate = false;
+        this.spriteTimer = 0;
+        this.spriteInterval = 150;
+
+        // game
+        this.score = 0;
+        this.winningScore = 100;
+        this.lives = 5;
 
         this.mouse = {
             x: 0,
@@ -266,6 +356,8 @@ class Game {
     render (context, deltaTime) {
         this.planet.draw(context);
 
+        this.drawStatusText(context);
+
         this.player.draw(context);
         this.player.update();
 
@@ -280,15 +372,65 @@ class Game {
         })
 
         // periodically activate an enemy
-        if (this.enemyTimer < this.enemyInterval) {
-            this.enemyTimer += deltaTime;
-        } else {
-            this.enemyTimer = 0;
-            const enemy = this.getEnemy();
-            if (enemy) {
-                enemy.start();
+        if (!this.gameOver) {
+            if (this.enemyTimer < this.enemyInterval) {
+                this.enemyTimer += deltaTime;
+            } else {
+                this.enemyTimer = 0;
+                const enemy = this.getEnemy();
+                if (enemy) {
+                    enemy.start();
+                }
             }
         }
+
+        // periodically update sprites
+        if (this.spriteTimer < this.spriteInterval) {
+            this.spriteTimer += deltaTime;
+            this.spriteUpdate = false;
+        } else {
+            this.spriteTimer = 0;
+            this.spriteUpdate = true;
+        }
+
+        // win/lose condition
+        if (this.score >= this.winningScore || this.lives < 1) {
+            this.gameOver = true;
+        }
+    }
+
+    drawStatusText (context) {
+        // this is where to import google fonts 1:38:52
+        context.save();
+        context.textAlign = 'left'
+        context.font = '30px Impact';
+        context.fillText('Score: ' + this.score, 10, 30);
+
+        for (let i = 0; i < this.lives; i++) {
+            context.fillRect(10 + 15 * i, 50, 10, 15);
+            // loadingbar example
+            // context.fillRect(20 + 15 * i, 60, 15, 30);
+        }
+
+        if (this.gameOver) {
+            context.textAlign = 'center';
+            let title;
+            let subTitle;
+            
+            if (this.score >= this.winningScore) {
+                title = 'You win!';
+                subTitle = `Your score is ${this.score}!`
+            } else {
+                title = 'You lose!'
+                subTitle = `Try again, bitch!`
+            }
+
+            context.font = '100px Impact';
+            context.fillText(title, this.width * 0.5, 300);
+            context.font = '50px Impact';
+            context.fillText(subTitle, this.width * 0.5, 580);
+        }
+        context.restore();
     }
 
     calcAim (a, b) {
@@ -325,7 +467,14 @@ class Game {
 
     createEnemyPool () {
         for (let i = 0; i < this.numberOfEnemies; i++) {
-            this.enemyPool.push(new Enemy(this));
+            let randomNumber = Math.random();
+
+            // in 25% of cases create asteroid
+            if (randomNumber > 0.25) {
+                this.enemyPool.push(new Asteroid(this));
+            } else {
+                this.enemyPool.push(new LobsterMorph(this));
+            }
         }
     }
 
@@ -341,10 +490,14 @@ class Game {
 window.addEventListener('load', function () {
     const canvas = document.getElementById('canvas1');
     const ctx = canvas.getContext('2d');
-    canvas.width = 1000;
-    canvas.height = 1000;
+    canvas.width = 900;
+    canvas.height = 900;
     ctx.strokeStyle = 'white';
+    ctx.fillStyle = 'white';
     ctx.lineWidth = 2;
+    ctx.font = '50px Helvetica';
+    ctx.textAlign = 'center';
+    ctx.textBaseline = 'middle';
 
     const game = new Game(canvas);
 
@@ -360,3 +513,15 @@ window.addEventListener('load', function () {
 
     requestAnimationFrame(animate)
 })
+
+
+// Stretch goals:
+// - optimize game loops and renders such that some of them dont continuously runInContext
+// - add a health bar above enemies
+// - refactor classes into separate files
+// - give planet lives
+// - give player lives
+// - add flash animation on collision
+// - optimize peen rotation. probably dont need to recalc
+// - sprite animate the planet as lives are lost
+// - sprite animate the space ship as lives are lost
